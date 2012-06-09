6f9a4b30907ce9e296dbaa985133720669a28bb9
Jonathan Camenisch <jonathan@camenisch.net>
2012-06-09 14:36:52 -0500

    Add suggested library include syntax to util.js

--- extensions/util.js
+++ extensions/util.js
@@ -1,1 +1,5 @@
+// Uncomment to use convenience libraries in extensions:
+// var _ = require('underscore')._;
+// var Mustache = require('mustache');
+
 exports.render_if = require('render_if').render_if;
