5e856a1d596b21a866b5371b7ce17f06bc555dae
Jonathan Camenisch <jonathan@camenisch.net>
2012-06-09 14:15:37 -0500

    Refine code and comments in render_if extension

--- extensions/render_if.js
+++ extensions/render_if.js
@@ -1,25 +1,48 @@
 var tests = {
-  /* Add any additional tests here
+  /* Add any custom tests (conditions) here
    * Params can be passed from templates in the form
-   *    <pop:util:render_if [test_name]="[input]"
-   * to run the function [test_name], passing [input] as the one
-   * parameter.
+   * 
+   *    <pop:util:render_if my_condition="something special"
+   *    
+   * to run tests['my_condition']('something special', scope)
+   * where scope is the current Webpop template context.
    *
    * The function should return true or false to indicate the
    * verdict of the test.
    */
 
-  section: function(input) {
-    return input === section.slug || input === section.title;
+  /**
+   * Checks the current section against the parameter
+   * @param  {string} titleOrSlug - the slug or title of the section to compare
+   * @return {boolean} - true if the current section matches the parameter.
+   */
+  section: function(titleOrSlug) {
+    return titleOrSlug === section.slug || titleOrSlug === section.title;
   },
 
-  contains: function(input) {
+  /**
+   * Checks a collection for specified quantity of items
+   * @param  {string} input - expects a comparison statement
+   *   in the form 
+   *
+   *     '[<operator>][ ]<integer>[ ]<collection_identifier>'
+   *
+   *   where <operator> is '>', '>=', '=', '<', or '<=' (defaults to '>=' if ommited)
+   *   and <collection_identifier> is a collection identifier in the scope of the calling template.
+   *
+   *
+   * @param {object} scope - current Webpop template scope
+   *           
+   * Example invocation: '<pop:util:render_if contains=">4 photos">...</pop:util:render_if>
+   *   
+   * @return {boolean} - true if the number of items in the collection matches the given criteria.
+   */
+  contains: function(input, scope) {
     var
-      input = input.split(' '),                            // e.g.=> ['>=2', 'photos']
-      condition_words = input[0].match(/([^0-9]+)?(\d+)/), // e.g.=> ['>=2', '>=', '2', ...]
-      comparator = condition_words[1],                     // e.g.=> '>='
-      number = parseInt(condition_words[2]),               // e.g.=> 2
-      collection = scope[input[1]]                         // e.g.=> 'photos'
+      tokens = input.match(/([^0-9 ]+)? *(\d+) *(.+)/), // e.g.=> ['>= 2 photos', '>=', '2', 'photos' ...]
+      comparator = tokens[1],                           // e.g.=> '>='
+      number = parseInt(tokens[2]),                     // e.g.=> 2
+      collection = scope[tokens[3]]                     // e.g.=> 'photos'
     ;
 
     if (!(number && collection && collection.length)) return false;
@@ -35,7 +58,7 @@ var tests = {
 
 exports.render_if = function(params, enclosed, scope) {
   var do_render = true;
-  for (var key in params) do_render &= tests[key] && tests[key](params[key]);
+  for (var key in params) do_render &= tests[key] && tests[key](params[key], scope);
 
   return !!do_render && enclosed.render();
 }
