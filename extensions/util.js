9f82b3141c06d433692d11cb115873d6d49afe76
Jonathan Camenisch <jonathan@camenisch.net>
2012-08-14 12:44:13 -0500

    Add separate place for site-specific vs. boilerplate extensions

--- extensions/util.js
+++ extensions/util.js
@@ -1,9 +1,15 @@
-// WARNING: this version untested
- 
+/**
+ * Broadly useful (not site-specific) utility extensions defined here
+ * Add site-specific functions to site_util.js. They will be added on
+ * to the exports from util for convenience (so <pop:util:...> will be
+ * as verbose as template authors need to get).
+ */
+
 var _ = exports._ = require('underscore')._;
 
 // Uncomment to use convenience libraries in extensions:
 _(exports).extend(
 //  { Mustache: require('mustache') },
-  require('render_if')
+  require('render_if'),
+  require('site_util')
 )
